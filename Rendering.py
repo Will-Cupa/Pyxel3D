fd67f3f383c74b222381c85fc85a02502552f26d
Alain <77106918+Will-Cupa@users.noreply.github.com>
2024-06-28 11:23:14 +0200

    Update Rendering.py

--- Rendering.py
+++ Rendering.py
@@ -1,1 +1,19 @@
-import pyxel as pyx
+import pyxel
+
+
+WIDTH = 1920
+HEIGHT = 1080
+
+
+class App:
+    def __init__(self):
+        pyxel.init(WIDTH, HEIGHT, title="3D", fps=60, quit_key=pyxel.KEY_ESCAPE, display_scale=4)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+    	pass
+
+    def draw(self):
+        pyxel.cls(3)
+
+App()
